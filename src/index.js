92ce0f1174840ff398625d480e0c48055900e33d
Sara Vieira <hey@iamsaravieira.com>
2018-02-14 00:06:09 +0000

    add ssr styled-components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './style';
 import App from './components/app';
+import { h, render } from 'preact';
+import { default as renderToString } from 'preact-render-to-string';
+import { ServerStyleSheet } from 'styled-components'
 
-export default App;
+if(typeof window === undefined) {
+    const sheet = new ServerStyleSheet()
+    const html = renderToString(sheet.collectStyles(<App />))
+    const styleTags = sheet.getStyleTags() // or sheet.getStyleElement()
+}
+if (typeof window !== "undefined") {
+    render((
+        <App />
+    ), document.getElementById('root'));
+}
