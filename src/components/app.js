c56f05a1f20d68902c179d7f68edf764a81b6662
Sara Vieira <hey@iamsaravieira.com>
2018-02-21 13:24:06 +0000

    updates

--- src/components/app.js
+++ src/components/app.js
@@ -9,12 +9,12 @@ import Category from '../routes/category';
 
 const Main = styled.main`
 	min-height: 100vh
-`
+`;
 
 const Footer = styled.footer`
 	text-align: center;
 	margin-top: 40px;
-`
+`;
 
 export default class App extends Component {
 
@@ -29,14 +29,14 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-			<Main>
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Category path="/category/:category" />
-					<SearchRoute path="/search/:query" />
-				</Router>
-			</Main>
+				<Main>
+					<Header />
+					<Router onChange={this.handleRoute}>
+						<Home path="/" />
+						<Category path="/category/:category" />
+						<SearchRoute path="/search/:query" />
+					</Router>
+				</Main>
 				<Footer>Ninja Icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></Footer>
 			</div>
 		);
